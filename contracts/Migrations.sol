d47a690d7177b17929546edf65753cfeb348298a
Anurag Jain <ajainuary@gmail.com>
2019-09-12 22:06:33 +0530

    Fixing extra scripts

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,15 +1,15 @@
-pragma solidity >=0.4.22 <0.6.0;
+pragma solidity >=0.4.21 <0.6.0;
 
 contract Migrations {
   address public owner;
-  uint256 public last_completed_migration;
-
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
+  uint public last_completed_migration;
 
   constructor() public {
     owner = msg.sender;
+  }
+
+  modifier restricted() {
+    if (msg.sender == owner) _;
   }
 
   function setCompleted(uint completed) public restricted {
