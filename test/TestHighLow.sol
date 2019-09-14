b89ab587d61b81da52d7ee7ef677ebe3b54d32ce
Bhavi Dhingra <bhavi.dhingra@students.iiit.ac.in>
2019-09-15 00:09:05 +0530

    Added testNewCard()

--- test/TestHighLow.sol
+++ test/TestHighLow.sol
@@ -4,8 +4,16 @@ import "truffle/Assert.sol";
 import "../contracts/HighLow.sol";
 
 contract TestHighLow {
-    function testSettingAnOwnerDuringCreation() public {
+    function testHouseDuringCreation() public {
         HighLow high_low = new HighLow();
         Assert.equal(address(high_low.house()), address(this), "An owner is different than a deployer");
+    }
+
+    function testNewCard() public {
+        HighLow high_low = new HighLow();
+        uint index = high_low.curr_card_index();
+        uint new_card = high_low.cards(index);
+        Assert.isAtLeast(new_card, 0, "New card is lower than zero");
+        Assert.isBelow(new_card, high_low.MAX_CARDS(), "New card is higher than max cards");
     }
 }
