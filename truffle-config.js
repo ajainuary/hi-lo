f3e2636882cd31f06ac5564f27b1b8e1ec98a27a
Anurag Jain <ajainuary@gmail.com>
2019-11-28 14:12:48 +0530

    Revert "Creating Frontend"

--- truffle-config.js
+++ truffle-config.js
@@ -23,7 +23,6 @@
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
-const path = require("path");
 
 module.exports = {
   /**
@@ -35,7 +34,7 @@ module.exports = {
    *
    * $ truffle test --network <network-name>
    */
-  contracts_build_directory: path.join(__dirname, "app/src/contracts"),
+
   networks: {
     // Useful for testing. The `development` name is special - truffle uses it by default
     // if it's defined here and no other network is specified at the command line.
